ba5446de17fc04c95523aa33033042a4abcbe0bc
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-08-30 03:45:40 +0800

    Add new file

--- mine.lua
+++ mine.lua
@@ -1,39 +1,10 @@
 local Mine = {}
 local Vec3 = require("vector3")
 local MoveHelper = require("move_helper")
-
----@type table<string, boolean>
-Mine.coalList = {
-    ["minecraft:coal"] = true,
-    ["minecraft:coal_block"] = true,
-    ["aether:ambrosium_shard"] = true
-}
+local RefuelHelper = require("refuel_helper")
 
 ---@type Vec3
 Mine.initPos = Vec3(0, 0, 0)
-
----@return boolean
-function Mine:refuel()
-    local fuelLevel = turtle.getFuelLevel()
-    if fuelLevel == "unlimited" or fuelLevel > 100 then return false end
-
-    for slot = 1, 16 do
-        local item = turtle.getItemDetail(slot)
-
-        if item and self[item.name] then
-            turtle.select(slot)
-            turtle.refuel(1)
-            print("Refueled with coal. Current fuel level: " .. turtle.getFuelLevel())
-            return true
-        end
-    end
-
-    print("Out of fuel! Please add coal to the turtle.")
-    while true do
-        sleep(10)
-        self:refuel()
-    end
-end
 
 function Mine:generateSphere(radius)
     local points = {}
@@ -60,7 +31,7 @@ local testStep = 0
 local initDirection = MoveHelper.directions.north
 
 function Mine:tick()
-    self:refuel()
+    RefuelHelper:refuel()
 
     if testStep > 1 then
         if MoveHelper.direction ~= initDirection then
---
+++ refuel_helper.lua
@@ -0,0 +1,63 @@
+local RefuelHelper = {}
+
+---@enum RefuelHelper.status
+RefuelHelper.status = {
+    idle = 0,
+    refueling = 1,
+    outOfFuel = 2
+}
+
+---@type RefuelHelper.status
+RefuelHelper.currentStatus = RefuelHelper.status.idle
+
+---@type table<string, boolean>
+RefuelHelper.coalList = {
+    ["minecraft:coal"] = true,
+    ["minecraft:coal_block"] = true,
+    ["aether:ambrosium_shard"] = true,
+    ["aether:ambrosium_block"] = true
+}
+
+---@return nil
+function RefuelHelper:refuel()
+    local fuelLevel = turtle.getFuelLevel()
+    if fuelLevel == "unlimited" or (fuelLevel > 100 and self.currentStatus == self.status.idle) then return end
+
+    if self.currentStatus == self.status.idle then
+        self.currentStatus = RefuelHelper.status.refueling
+    end
+
+    for slot = 1, 16 do
+        local item = turtle.getItemDetail(slot)
+
+        if item and self.coalList[item.name] then
+            turtle.select(slot)
+            while turtle.getItemCount(slot) > 0 do
+                local fuelLevel = turtle.getFuelLevel()
+                turtle.refuel(1)
+                print("Refueled with " .. item.name .. ". Current fuel level: " .. fuelLevel)
+
+                if fuelLevel > 1000 then
+                    self.currentStatus = RefuelHelper.status.idle
+                    return
+                end
+                sleep(0)
+            end
+        end
+    end
+
+    local fuelLevel = turtle.getFuelLevel()
+    if fuelLevel > 100 then
+        self.currentStatus = RefuelHelper.status.idle
+        return
+    end
+
+    print("Out of fuel! Please add coal to the turtle.")
+    self.currentStatus = RefuelHelper.status.outOfFuel
+    while true do
+        sleep(10)
+        self:refuel()
+    end
+end
+
+return RefuelHelper
