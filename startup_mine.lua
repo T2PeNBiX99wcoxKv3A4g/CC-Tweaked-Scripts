d0a1e185f603eda64022f6573f68b0cc68cde2c7
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-08-30 12:03:41 +0800

    Change require order

--- startup_mine.lua
+++ startup_mine.lua
@@ -1,12 +1,14 @@
 -- Rename to startup.lua to run at startup
 
 ---@diagnostic disable: lowercase-global
-moveHelper = require("move_helper")
-refuelHelper = require("refuel_helper")
+
+vec3 = require("vector3")
+hook = require("hook")
 logHelper = require("log_helper")
 saveHelper = require("save_helper")
-hook = require("hook")
-vec3 = require("vector3")
+refuelHelper = require("refuel_helper")
+moveHelper = require("move_helper")
+
 ---@diagnostic enable: lowercase-global
 
 local mine = require("mine")
