b0a935b8555a5abfd2c87c4a3781f962a70db2d3
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-09-01 05:14:21 +0800

    Update utils

--- utils.lua
+++ utils.lua
@@ -1,4 +1,19 @@
+---@class utils
 local utils = {}
+
+---@enum utils.computerTypes
+utils.computerTypes = {
+    normal = 0,
+    turtle = 1,
+    pocket = 2
+}
+
+---@return utils.computerTypes
+function utils.computerType()
+    if turtle then return utils.computerTypes.turtle end
+    if pocket then return utils.computerTypes.pocket end
+    return utils.computerTypes.normal
+end
 
 utils.__oldPrint = print
 
@@ -10,11 +25,10 @@ function print(...)
         if type(value) == "table" then
             table.insert(rets, string.format("%s - %s", tostring(value), textutils.serialise(value)))
         else
-            table.insert(rets, tostring(value))
+            table.insert(rets, value)
         end
     end
-
-    utils.__oldPrint(table.concat(rets, "\t"))
+    utils.__oldPrint(table.unpack(rets))
 end
 
 return utils
