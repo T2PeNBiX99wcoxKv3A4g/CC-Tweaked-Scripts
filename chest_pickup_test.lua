be4949747926b83bcc76779fab2e15c3dadb2e98
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-08-31 11:33:37 +0800

    Update test code

--- chest_pickup_test.lua
+++ chest_pickup_test.lua
@@ -4,9 +4,16 @@ function pickUp:init()
     local test = peripheral.find("inventory")
     if not test then return end
 
+    turtle.turnLeft()
+    turtle.turnLeft()
+
     for slot, item in pairs(test.list()) do
         print(("%d x %s in slot %d"):format(item.count, item.name, slot))
+        turtle.suck()
     end
+
+    turtle.turnLeft()
+    turtle.turnLeft()
 end
 
 pickUp:init()
